79913a429ae4273b2f452bbe6dc1f71b72118da3
Bryan Eduardo Barba Estrada <u202323479@upc.edu.pe>
2026-05-11 14:39:39 -0500

    refactor(campaign-form): reorganize imports in create-campaign component

--- src/app/campaign/presentation/views/crear-campaign/crear-campaign.component.ts
+++ src/app/campaign/presentation/views/crear-campaign/crear-campaign.component.ts
@@ -2,7 +2,16 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
-import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ValidatorFn,
+  AbstractControl,
+  ValidationErrors
+} from '@angular/forms';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
